49395e687f31bddbad96c8fb37e682424d9b686b
Bryan Schmidt <bryanschmidt1992@gmail.com>
2019-06-04 01:54:54 -0700

    Finish initial capability of tile map drawing

--- Tile.m
+++ Tile.m
@@ -4,44 +4,59 @@ classdef Tile
     
     properties
         LocationCenter = [0 0];
-        LocationEdges = [0 0;
-            0 0;
-            0 0;
-            0 0;
-            0 0];
+        WallVector = [{[0 0; 0 0]};
+            {[0 0; 0 0]};
+            {[0 0; 0 0]};
+            {[0 0; 0 0]}];
         Walls = 0; % Binary 0000 each bit represents a wall
         % 0 - Wall
         % 1 - No Wall
+        PolyX = [];
+        PolyY = [];
     end
     
     methods
         function obj = CreateCell(obj,CenterX,CenterY)
             obj.LocationCenter = [CenterX, CenterY];
-            obj.LocationEdges = [CenterX-0.5 CenterY-0.5;
-                CenterX+0.5 CenterY-0.5;
-                CenterX+0.5 CenterY+0.5;
-                CenterX-0.5 CenterY+0.5;
-                CenterX-0.5 CenterY-0.5];
+            obj.WallVector{1} = [CenterX-0.5 CenterY-0.5; CenterX+0.5 CenterY-0.5];
+            obj.WallVector{2} = [CenterX+0.5 CenterY-0.5; CenterX+0.5 CenterY+0.5];
+            obj.WallVector{3} = [CenterX+0.5 CenterY+0.5; CenterX-0.5 CenterY+0.5];
+            obj.WallVector{4} = [CenterX-0.5 CenterY+0.5; CenterX-0.5 CenterY-0.5];
+            
+            for i = 1:length(obj.WallVector)
+                obj.PolyX(end+1) = obj.WallVector{i}(1,1);
+                obj.PolyY(end+1) = obj.WallVector{i}(1,2);
+            end
         end
+        
         function obj = RemoveWall(obj,direction)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
             obj.Walls = bitor(obj.Walls,direction);
             switch direction
-                case 1 % North
-                    obj.LocationEdges = obj.LocationEdges
-                case 2 % East
-                case 4 % South
-                case 8 % West
+                case 0 % North
+                    obj.WallVector{3} = [];
+                case 1 % East
+                    obj.WallVector{2} = [];
+                case 2 % South
+                    obj.WallVector{1} = [];
+                case 3 % West
+                    obj.WallVector{4} = [];
             end
         end
         
         function DisplayWall(obj)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
-            fill(obj.LocationEdges(1,:),obj.LocationEdges(2,:),'k')
-            plot(obj.LocationCenter(1),obj.LocationCenter(2),'oc','MarkerFaceColor','w','MarkerSize',2)
-            plot(obj.LocationEdges(1,:),obj.LocationEdges(2,:),'c','LineWidth',2)
+            
+            fill(obj.PolyX,obj.PolyY,'k')
+            for i = 1:length(obj.WallVector)
+                if ~isempty(obj.WallVector{i})
+                    plot(obj.WallVector{i}(:,1),obj.WallVector{i}(:,2),'c','LineWidth',2)
+                end
+            end
+            plot(obj.LocationCenter(1),obj.LocationCenter(2),'oc','MarkerFaceColor','w','MarkerSize',6)
+            
         end
     end
 end
--- TileMapCreation.m
+++ TileMapCreation.m
@@ -2,10 +2,17 @@ clearvars
 close all
 clc
 
-GridSizeX = 20;
-GridSizeY = 20;
+GridSizeX = 10;
+GridSizeY = 10;
 
-% Map = zeros(GridSizeX,GridSizeY);
+%- Direction Vectors
+N = {[0 1]};
+E = {[1 0]};
+S = {[0 -1]};
+W = {[-1 0]};
+keySet = {0, 1, 2, 3};
+valueSet = [N, E, S, W];
+dir = containers.Map(keySet,valueSet);
 
 figure
 hold on
@@ -15,6 +22,48 @@ for i = 1:GridSizeX
     for j = 1:GridSizeY
         Map(i,j) = Tile;
         Map(i,j) = CreateCell(Map(i,j),i-0.5,j-0.5);
+    end
+end
+
+for i = 1:GridSizeX
+    for j = 1:GridSizeY
+        temp = rand();
+        if (temp < 0.2)
+            direction = 0;
+        elseif (temp < 0.4)
+            direction = 2;
+        elseif (temp < 0.7)
+            direction = 1;
+        elseif (temp < 1.0)
+            direction = 3;
+        end
+        switch direction
+            case 0
+                if (j+1 <= GridSizeY)
+                    Map(i,j) = RemoveWall(Map(i,j),0);
+                    Map(i,j+1) = RemoveWall(Map(i,j+1),2);
+                    DisplayWall(Map(i,j+1))
+                end
+            case 1
+                if (i+1 <= GridSizeX)
+                    Map(i,j) = RemoveWall(Map(i,j),1);
+                    Map(i+1,j) = RemoveWall(Map(i+1,j),3);
+                    DisplayWall(Map(i+1,j))
+                end
+            case 2
+                if (j-1 > 0)
+                    Map(i,j) = RemoveWall(Map(i,j),2);
+                    Map(i,j-1) = RemoveWall(Map(i,j-1),0);
+                    DisplayWall(Map(i,j-1))
+                end
+            case 3
+                if (i-1 > 0)
+                    Map(i,j) = RemoveWall(Map(i,j),3);
+                    Map(i-1,j) = RemoveWall(Map(i-1,j),1);
+                    DisplayWall(Map(i-1,j))
+                end
+        end
         DisplayWall(Map(i,j))
     end
 end
+
